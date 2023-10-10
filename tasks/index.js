229a1441ecd50bf560a9de4c4aaf7fe407f7418c
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-10-10 23:47:42 +0530

    add init

--- tasks/index.js
+++ tasks/index.js
@@ -1,2 +1,6 @@
 exports.deploy = require("./deploy");
 exports.verifyContract = require("./verify");
+exports.createEvent = require("./createEvent");
+exports.init = require("./init");
+exports.buyTicket = require("./buyTicket");
+exports.tipEvent = require("./tipEvent");
---
+++ tasks/init/index.js
@@ -0,0 +1,4 @@
+exports.initDistributor = require("./initDistributor");
+exports.initLiveTipping = require("./initLiveTipping");
+exports.initStreamStorage = require("./initStreamStorage");
+exports.initTicketFactory = require("./initTicketFactory");
